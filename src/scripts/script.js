d27e6769f0749b34d0f5636fd47dc2d1f2f057a2
Niewolinsky <niewolinski@protonmail.com>
2023-02-02 18:07:54 +0100

    Added Amazon SES+Lambda+API Gateway simple contact form submission function

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,3 +9,39 @@ toggleSwitch.onchange = function (e) {
         document.documentElement.setAttribute('data-theme', 'light')
     }
 }
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    // prevent the form submit from refreshing the page
+    event.preventDefault();
+
+    const { name, email, message } = event.target;
+
+    // Use your API endpoint URL you copied from the previous step
+    const endpoint =
+        "<https://w3xi70pyx9.execute-api.eu-central-1.amazonaws.com/default/sendEmailNode>";
+    // We use JSON.stringify here so the data can be sent as a string via HTTP
+    const body = JSON.stringify({
+        senderName: name.value,
+        senderEmail: email.value,
+        message: message.value
+    });
+    const requestOptions = {
+        method: "POST",
+        body
+    };
+
+    fetch(endpoint, requestOptions)
+        .then((response) => {
+            if (!response.ok) throw new Error("Error in fetch");
+            return response.json();
+        })
+        .then((response) => {
+            document.getElementById("result-text").innerText =
+                "Email sent successfully!";
+        })
+        .catch((error) => {
+            document.getElementById("result-text").innerText =
+                "An unkown error occured.";
+        });
+});
