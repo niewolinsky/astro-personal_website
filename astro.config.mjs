3b284366f4675a8d31ff34b0b8e431328aebb70a
Niewolinsky <niewolinski@protonmail.com>
2023-02-02 18:16:08 +0100

    Github Pages integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://niewolinsky.github.io',
+	base: '/astro-personal_website',
 	integrations: [mdx(), sitemap()],
 });
