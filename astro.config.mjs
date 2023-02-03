2de2ecfef1d9726adbe30c2e925e6b80c6cbdfa6
Przemysław Niewoliński <niewolinski@protonmail.com>
2023-02-03 13:39:21 +0100

    Update to my domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://niewolinsky.github.io',
-	base: '/astro-personal_website',
+	site: 'https://niewolinsky.dev',
 	integrations: [mdx(), sitemap()],
 });
