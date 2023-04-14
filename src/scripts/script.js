df4d261c4cf96fe71f7a6d713e079934a87d603b
Niewolinsky <niewolinski@protonmail.com>
2023-04-14 13:07:58 +0200

    fixed darkmode toggle and content jumping on image load

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,8 +4,20 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]') //ELEMENT:
 toggleSwitch.onchange = function (e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light');
     }
 }
+
+function setThemeFromLocalStorage() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      document.documentElement.setAttribute('data-theme', savedTheme);
+      toggleSwitch.checked = savedTheme === 'dark';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', setThemeFromLocalStorage);
